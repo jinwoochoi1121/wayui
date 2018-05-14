bab9caa7cb076ef35c147ee5469e3c6d011550fa
jinwoochoi1121 <39250131+jinwoochoi1121@users.noreply.github.com>
2018-05-14 10:25:15 +0900

    Add files via upload
    jkhkjkjv

---
+++ WayZainer UI/WayZainer UI/MF_1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * SharpDevelop으로 작성되었습니다.
+ * 사용자: jin
+ * 날짜: 05/11/2018
+ * 시간: 15:56
+ * 
+ * 이 템플리트를 변경하려면 [도구->옵션->코드 작성->표준 헤더 편집]을 이용하십시오.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WayZainer_UI
+{
+	/// <summary>
+	/// Interaction logic for MF_1.xaml
+	/// </summary>
+	public partial class MF_1 : Window
+	{
+		public MF_1()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ WayZainer UI/WayZainer UI/SF_1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * SharpDevelop으로 작성되었습니다.
+ * 사용자: jin
+ * 날짜: 2018-05-10
+ * 시간: 오후 4:56
+ * 
+ * 이 템플리트를 변경하려면 [도구->옵션->코드 작성->표준 헤더 편집]을 이용하십시오.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WayZainer_UI
+{
+	/// <summary>
+	/// Interaction logic for Subform.xaml
+	/// </summary>
+	public partial class Subform : Window
+	{
+		public Subform()
+		{
+			InitializeComponent();
+		}
+	}
+}
